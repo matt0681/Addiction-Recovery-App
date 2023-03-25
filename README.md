a9a93918c3e28551b5aceb4c97bbbea1c6e0b768
Matthew Lad <mattman678@gmail.com>
2023-03-25 18:10:29 -0400

    Finished the splash screen. Organized the code more.

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 This is a personal project (not school or work affiliated) to create an addiction recovery / sobriety tracking app. The inspiration behind this app is the quote: "You are worth it. You are loved," as well as the desire to help those in my community effected by substance abuse.
 
-<a href="App's Home Page"><img src="/HomePageScreenShot.PNG" height="525" width="294" ></a>
+<a href="App's Home Page"><img src="/assets/HomePageScreenShot.PNG" height="525" width="294" ></a>
 
 Currently a work in progress!
 
--- lib/main.dart
+++ lib/main.dart
@@ -42,6 +42,7 @@ class MyApp extends StatelessWidget {
           textTheme: Typography.whiteCupertino,
         ),
         home: MyHomePage(),
+        // home: InitialPage(),
       ),
     );
   }
@@ -63,8 +64,8 @@ class MyAppState extends ChangeNotifier {
   //   notifyListeners();
   // }
 }
-
-
+//
+//
 // /// The initial 'loading screen' of the app.
 // /// Consists of our motto along with our name.
 // class InitialPage extends StatefulWidget {
@@ -131,4 +132,4 @@ class MyAppState extends ChangeNotifier {
 //     );
 //   }
 // }
-
+//
