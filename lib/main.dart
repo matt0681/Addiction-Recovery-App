ccef448dc801c6eddcf26be74f5519a2fbb210a6
Matthew Lad <mattman678@gmail.com>
2023-04-14 21:24:46 -0400

    started theme overhaul. Better theme and look. also better organization.

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,9 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:noe_more/screens/home_page.dart';
+import 'package:noe_more/screens/resources_page.dart';
+import 'package:noe_more/screens/settings_page.dart';
+import 'package:noe_more/screens/tracker_page.dart';
 
 
 /// This runs the MyApp Widget. It will load the entire app before letting the
@@ -32,8 +35,62 @@ class MyApp extends StatelessWidget {
         fontFamily: 'Inter',
       ),
 
-      home: MyHomePage(),
+      home: HomePage(),
     );
   }
 }
 
+/// This page contains the bottom navigation bar and displays which ever
+/// page is currently selected.
+class HomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<MyHomePage> {
+
+  int _selectedIndex = 0;
+
+  final List<Widget> _widgetOptions = <Widget>[
+    TrackerPage(),
+    ResourcesPage(),
+    SettingsPage()
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      body: _widgetOptions.elementAt(_selectedIndex),
+
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.event),
+            label: 'Tracker',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.local_library),
+            label: 'Resources',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ],
+
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.white,
+        backgroundColor: Color(0xFF2A5298),
+        unselectedItemColor: Color(0xFF7D91BA),
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
