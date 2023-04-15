7c4350cd93201e934c5a9562bfb23e1307ec14cf
Matthew Lad <mattman678@gmail.com>
2023-04-15 17:01:51 -0400

    Update resource cards and settings page.

--- lib/screens/resources_page.dart
+++ lib/screens/resources_page.dart
@@ -2,7 +2,35 @@ import 'package:flutter/material.dart';
 
 
 class ResourcesPage extends StatelessWidget {
-  const ResourcesPage({super.key});
+
+  final List<List<String>> _resourceList = [
+    ["Classic AA Book", "Used by all North American AA groups."],
+    ["North American AA Official Website", "Has helped Alcoholics recover since 1995."]
+  ];
+
+  Widget _defaultListView() {
+    return ListView.builder(
+      itemCount: _resourceList.length,
+      itemBuilder: (context, index) {
+        return Card(
+          color: Color(0xFF055680),
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(10.0))),
+          margin: EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 0.0),
+
+          child: ListTile(
+            leading: FlutterLogo(size: 50.0),
+            title: Text(
+                _resourceList[index][0],
+                style: TextStyle(color: Colors.white)),
+            subtitle: Text(
+              _resourceList[index][1],
+              style: TextStyle(color: Color(0xFF7D91BA)),
+            ),
+          ),
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,48 +38,7 @@ class ResourcesPage extends StatelessWidget {
 
       backgroundColor: Color(0xFF06294A),
 
-      body: ListView(
-        children: const <Widget>[
-
-
-          Card(
-            color: Color(0xFF055680),
-            shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(10.0))),
-            margin: EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 0.0),
-
-            child: ListTile(
-              leading: FlutterLogo(size: 50.0),
-              title: Text(
-                  'Classic AA Book',
-                  style: TextStyle(color: Colors.white)),
-              subtitle: Text(
-                  'Used by all North American AA groups.',
-                  style: TextStyle(color: Color(0xFF7D91BA)),
-              ),
-            ),
-          ),
-
-
-          Card(
-            color: Color(0xFF055680),
-            shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(10.0))),
-            margin: EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 0.0),
-
-            child: ListTile(
-              leading: FlutterLogo(size: 50.0),
-              title: Text(
-                  'North American AA Official Website',
-                  style: TextStyle(color: Colors.white)),
-              subtitle: Text(
-                'Has helped Alcoholics recover since 1995.',
-                style: TextStyle(color: Color(0xFF7D91BA)),
-              ),
-            ),
-          ),
-
-
-        ],
-      ),
+      body: _defaultListView(),
     );
   }
 }
--- lib/screens/settings_page.dart
+++ lib/screens/settings_page.dart
@@ -7,7 +7,42 @@ class SettingsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("Hello There! SETTINGS PAGE"),
+      appBar: AppBar(
+        title: const Text(
+            'Settings',
+            style: TextStyle(color: Colors.white),
+        ),
+
+
+
+        backgroundColor: Color(0xFF06294A),
+        elevation: 0.0,
+      ),
+
+      backgroundColor: Color(0xFF06294A),
+
+      body: ListView(
+        children: const <Widget>[
+
+          Card(
+            color: Color(0xFF055680),
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(10.0))),
+            margin: EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 0.0),
+
+            child: ListTile(
+              leading: FlutterLogo(size: 50.0),
+              title: Text(
+                  'Classic AA Book',
+                  style: TextStyle(color: Colors.white)),
+              subtitle: Text(
+                'Used by all North American AA groups.',
+                style: TextStyle(color: Color(0xFF7D91BA)),
+              ),
+            ),
+          ),
+
+        ],
+      )
     );
   }
 }
