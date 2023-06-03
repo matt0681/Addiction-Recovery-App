dc386482dd5d7f3ed6ce3681aa193cf57e0890f0
Matthew Lad <mattman678@gmail.com>
2023-06-03 18:16:12 -0500

    Added comments to the about page.

--- lib/screens/about_page.dart
+++ lib/screens/about_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
-
+///-----!!!!------This page is currently not in use-----!!!!------\\\
+/// This page is used to provide the user some information about the app's
+/// creators, some contact information, and
 class AboutPage extends StatelessWidget {
   const AboutPage({super.key});
 
@@ -11,3 +13,4 @@ class AboutPage extends StatelessWidget {
     );
   }
 }
+
